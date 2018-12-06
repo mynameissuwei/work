e666a5301292279e406e42b6e480b8a6408f0f00
suwei <910933955@qq.com>
2018-12-06 23:20:35 +0800

    finishing

--- chat-react/src/components/msg/msg.js
+++ chat-react/src/components/msg/msg.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { List } from 'antd-mobile'
+import { List, Badge } from 'antd-mobile'
 import { getLast } from '../../util'
 
 @connect(state => state)
@@ -19,16 +19,33 @@ class Msg extends React.Component {
     })
     const Item = List.Item;
     const Brief = Item.Brief;
-    const value = Object.values(user)
+
+    const value = Object.values(user).sort( (a,b) => {
+      const a_last = getLast(a).create_time
+      const b_last = getLast(b).create_time
+      return b_last - a_last
+    })
+
+    const userId = this.props.user._id
     return (
       <div>
         {value.map(v => {
+          console.log(v)
           const item = getLast(v)
+          const userIdCard =  item.from == userId ? item.from : item.to
+          const unreadNum = v.filter(v => !v.read && v.to==userId).length
           return (
             <List key={item._id}>
-              <Item>
+              <Item
+                extra={<Badge text={unreadNum}></Badge>}
+                arrow='horizontal'
+                onClick={() => {
+                  this.props.history.push(`/chat/${userIdCard}`)
+                  window.location.href = window.location.href
+                }}
+              >
                 {item.content}
-                <Brief>王欢</Brief>
+                <Brief>{this.props.chat.users[userIdCard].name}</Brief>
               </Item>
     
             </List>
---
+++ question.mk
@@ -0,0 +1,2 @@
+react
+	arrow function 返回对象必须用括号包起来 , 如果不用括号抱起来 , 里面没有return 东西
